c0d589115804ee0e57ea89cd75417c467ebc7d7d
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-02-16 19:05:06 -0800

    routing-Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div>Hello</div>
+  return (
+    <div className={'flex flex-col gap-4 p-10'}>
+      <Link href={'./auth'}>Auth</Link>
+      <Link href={'./auth/login'}>Login</Link>
+      <Link href={'./auth/sign-up'}>SignUp</Link>
+    </div>
+  )
 }
